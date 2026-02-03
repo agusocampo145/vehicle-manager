e58e8aa537dc0533854b7c0671d4c7b524cfa655
Agustin Ocampo <oca_a@outlook.com.ar>
2026-02-02 23:28:32 -0300

    Exceptions

---
+++ src/main/java/com/kavak/vehicle_manager/exceptions/RecursoNoEncontradoException.java
@@ -0,0 +1,7 @@
+package com.kavak.vehicle_manager.exceptions;
+
+public class RecursoNoEncontradoException extends RuntimeException {
+    public RecursoNoEncontradoException(String mensaje) {
+        super(mensaje);
+    }
+}
---
+++ src/main/java/com/kavak/vehicle_manager/exceptions/ReglaDeNegocioException.java
@@ -0,0 +1,7 @@
+package com.kavak.vehicle_manager.exceptions;
+
+public class ReglaDeNegocioException extends RuntimeException {
+    public ReglaDeNegocioException(String mensaje) {
+        super(mensaje);
+    }
+}
